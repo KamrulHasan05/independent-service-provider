00c782e99534b9e87ffb6e7e00af735d0476dff7
Kamrul Hasan <mdkamrulhasan.du@gmail.com>
2022-04-18 09:03:54 +0600

    about page done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 <h2 className='text-white'>This is About Me Page</h2>
             </div>
             <div className="container">
-                <div className="row pt-5 g-5">
+                <div className="row pt-5 g-5 align-items-center">
 
                     {/* Column-1 */}
                     <div className="col-md-6">
@@ -22,7 +22,7 @@ const About = () => {
                         <div className="about-me">
                             <h3 className='mb-3'>Md Kamrul Hasan</h3>
                             <p className='mb-3'>Every person has a dream or goal. As a human being or person, I have a goal that I will be pro coder. Front-end and Back-end both will be easy for me. I have another goal to do remote job. I am waiting when goal will come true.</p>
-                            <p></p>
+                            <p>I always try to do hard work. I am giving my maximum time behid the programing, because i have lots of time behid the programing. If need much time, I will give to acheive my goal.</p>
                         </div>
                     </div>
 
