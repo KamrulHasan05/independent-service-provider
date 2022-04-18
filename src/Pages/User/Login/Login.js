adfc18f712fdb97fa0e16536a213d314ee8daa7d
Kamrul Hasan <mdkamrulhasan.du@gmail.com>
2022-04-18 23:22:20 +0600

    project done

--- src/Pages/User/Login/Login.js
+++ src/Pages/User/Login/Login.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from './../../../firbase.init';
 import Loading from '../../Shared/Loading/Loading';
+import { FaGoogle } from "react-icons/fa";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const [
@@ -12,15 +17,35 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+
+    const [signInWithGoogle, googleUser, GoogleLoading, GoogleError] = useSignInWithGoogle(auth);
+
+
+    const [sendPasswordResetEmail, sending, resetError] = useSendPasswordResetEmail(auth);
+
+    const [email, setEmail] = useState('')
+
+    const handleEmail = event => {
+        setEmail(event.target.value)
+    }
+
     const navigate = useNavigate()
     const location = useLocation()
     const from = location?.state?.from?.pathname || '/'
-    if (loading) {
+
+
+    if (loading || GoogleLoading) {
         return <Loading />
     }
+    if (sending) {
+        toast.success('Reset Email Sent')
+    }
     // navigate after login
-    if (user) {
+    if (user || googleUser) {
         navigate(from, { replace: true })
+    }
+    else if (user || googleUser) {
+        navigate('/')
     }
 
     const handleLogin = event => {
@@ -29,7 +54,15 @@ const Login = () => {
         const password = event.target.password.value
         signInWithEmailAndPassword(email, password)
 
+
     }
+    const handleResetPass = () => {
+        if (email) {
+            sendPasswordResetEmail(email)
+        }
+        toast.error('Please Enter your email')
+    }
+
     return (
         <section className='signup py-5'>
             <div className="container">
@@ -41,7 +74,7 @@ const Login = () => {
 
                                 <Form.Group className="mb-3" >
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control name='email' type="email" placeholder="Enter email address" required />
+                                    <Form.Control onBlur={handleEmail} name='email' type="email" placeholder="Enter email address" required />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" >
@@ -54,8 +87,16 @@ const Login = () => {
                                 </Button>
                             </Form>
                             <div className='mt-3'>
+                                <p><Link to="#" onClick={handleResetPass} className='text-primary pe-auto text-decoration-none'>Forgetted Password?</Link> </p>
+                            </div>
+                            <div className='mt-3'>
                                 <p>Are you new here? <Link to="/signup" className='text-primary pe-auto text-decoration-none'>Please Sign up</Link> </p>
                             </div>
+
+                            <div className='text-center mt-5'>
+                                <button className='btn btn-primary w-100' onClick={() => signInWithGoogle()}><FaGoogle className='me-3' /> Log in with Google</button>
+                            </div>
+                            <ToastContainer />
                         </div>
                     </div>
                 </div>
--- src/Pages/User/SignUp/SignUp.js
+++ src/Pages/User/SignUp/SignUp.js
@@ -2,30 +2,36 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import Loading from '../../Shared/Loading/Loading';
 import './SignUp.css'
-import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from './../../../firbase.init';
 import { useNavigate, Link } from 'react-router-dom';
-import { async } from '@firebase/util';
+import { FaGoogle } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignUp = () => {
     const [agree, setAgree] = useState(false);
+    const [error, setError] = useState('')
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
-        error,
+        createUserError,
     ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const [signInWithGoogle, googleUser, GoogleLoading, GoogleError] = useSignInWithGoogle(auth);
 
     const navigate = useNavigate()
-
-    if (loading || updating) {
+    if (createUserError || GoogleError || updateError) {
+        toast.error(`${createUserError ? createUserError.message : ''}${GoogleError ? GoogleError.message : ''}${updateError ? updateError.message : ''}`)
+    }
+    if (loading || updating || GoogleLoading) {
         return <Loading />;
     }
 
-    if (user) {
+    if (user || googleUser) {
         navigate('/')
         console.log(user);
     }
@@ -34,9 +40,15 @@ const SignUp = () => {
         const name = event.target.name.value
         const email = event.target.email.value
         const password = event.target.password.value
-        await createUserWithEmailAndPassword(email, password);
-        await updateProfile({ displayName: name });
-
+        const confirmPassword = event.target.confirmPassword.value
+        if (password === confirmPassword) {
+            await createUserWithEmailAndPassword(email, password);
+            await updateProfile({ displayName: name });
+            setError('');
+        }
+        else {
+            setError("password no match")
+        }
     }
 
     return (
@@ -62,6 +74,16 @@ const SignUp = () => {
                                     <Form.Control name='password' type="password" placeholder="Password" required />
                                 </Form.Group>
 
+                                <Form.Group className="mb-3" >
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control name='confirmPassword' type="password" placeholder="Retype your Password" required />
+                                    {error &&
+                                        <Form.Text className=" text-danger ">
+                                            <p className='mt-3'>{error}</p>
+                                        </Form.Text>
+                                    }
+                                </Form.Group>
+
                                 <Form.Group className="mb-3">
                                     <Form.Check onClick={() => setAgree(!agree)} name='agree' type="checkbox" label="Agree with terms and conditions" />
                                 </Form.Group>
@@ -73,6 +95,11 @@ const SignUp = () => {
                             <div>
                                 <p>Already have an account? <Link to="/login" className='text-primary pe-auto text-decoration-none'>Please Login</Link> </p>
                             </div>
+
+                            <div className='text-center mt-5'>
+                                <button className='btn btn-primary w-100' onClick={() => signInWithGoogle()}><FaGoogle className='me-3' /> Log in with Google</button>
+                            </div>
+                            <ToastContainer />
                         </div>
                     </div>
                 </div>
