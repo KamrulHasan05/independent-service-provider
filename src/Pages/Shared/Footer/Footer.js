ee134ef0e33ee2bcb061276ccee359affe5471f3
Kamrul Hasan <mdkamrulhasan.du@gmail.com>
2022-04-18 09:19:18 +0600

    footer section done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer className='py-5 bg-success'>
+            <div className="container">
+                <h3 className='fs-4 text-center mb-0 text-white'>&copy; Learn With Rony || All Rights Reserve By Rony {new Date().getUTCFullYear()}</h3>
+            </div>
+
+        </footer>
     );
 };
 
